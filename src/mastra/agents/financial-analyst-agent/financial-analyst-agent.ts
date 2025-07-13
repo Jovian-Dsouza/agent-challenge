381f7e2bc911fb784beef9d303b4ff735cbc76ab
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2025-07-13 11:09:02 +0530

    feat: add mcp server

--- src/mastra/agents/financial-analyst-agent/financial-analyst-agent.ts
+++ src/mastra/agents/financial-analyst-agent/financial-analyst-agent.ts
@@ -2,6 +2,8 @@ import { Agent } from "@mastra/core/agent";
 import { model } from "../../config";
 // import { stockPrices } from "../financial-analyst-agent/financial-analyst-tools";
 import { mcp } from "../financial-analyst-agent/financial-analyst-mcps";
+import { MCPServer } from "@mastra/mcp";
+import http from "http";
 
 // Define instructions for the agent
 const instructions = `
@@ -40,9 +42,35 @@ const instructions = `
 }
 `;
 
+const description = "A Financial Analyst AI agent that answers user questions by intelligently selecting and using finance and web tools to retrieve, analyze, and synthesize financial information"
+
 export const financialAnalystAgent = new Agent({
 	name: "Financial Analyst Agent",
+	description,
 	instructions,
 	model,
 	tools: await mcp.getTools(),
+});
+
+const server = new MCPServer({
+	name: "My Custom Server",
+	version: "1.0.0",
+	// tools: await mcp.getTools(),
+	tools: {},
+	agents: { financialAnalystAgent }, // this agent will become tool "ask_myAgent"
+});
+
+const httpServer = http.createServer(async (req, res) => {
+	await server.startSSE({
+	  url: new URL(req.url || "", `http://localhost:1234`),
+	  ssePath: "/sse",
+	  messagePath: "/message",
+	  req,
+	  res,
+	});
+  });
+
+const PORT = 8081
+httpServer.listen(PORT, () => {
+  console.log(`===> HTTP server listening on port ${PORT}`);
 });
