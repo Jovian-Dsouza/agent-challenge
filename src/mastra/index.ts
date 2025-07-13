11a106097cd61278dbbe92c3b58816edecf58c82
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2025-07-13 14:46:46 +0530

    feat: update docker file

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,14 +1,11 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
-import { weatherAgent } from "./agents/weather-agent/weather-agent"; // This can be deleted later
-import { weatherWorkflow } from "./agents/weather-agent/weather-workflow"; // This can be deleted later
-import { yourAgent } from "./agents/your-agent/your-agent"; // Build your agent here
 import { financialAnalystAgent } from "./agents/financial-analyst-agent/financial-analyst-agent";
 import { financialAnalysisWorkflow } from "./agents/financial-analyst-agent/financial-analyst-workflow";
 
 export const mastra = new Mastra({
-	workflows: { weatherWorkflow, financialAnalysisWorkflow }, // can be deleted later
-	agents: { weatherAgent, yourAgent, financialAnalystAgent },
+	workflows: { financialAnalysisWorkflow }, // can be deleted later
+	agents: { financialAnalystAgent },
 	logger: new PinoLogger({
 		name: "Mastra",
 		level: "info",
